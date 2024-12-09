94ac4ce531ea6a0c4c4056904e6c2eb751ceeae6
haruto8610 <haruto.fukushina0207@gmail.com>
2024-12-09 15:09:05 +0900

    修正

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/app/components/Header";
+import Header from "@/components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
-        {children}
+        <main className="pt-[70px]">
+          {children}
+        </main>
+        
       </body>
     </html>
   );
