94368af9c9a8039f920ee79a4c349f6a65b72b9d
haruto8610 <haruto.fukushina0207@gmail.com>
2024-12-20 13:10:44 +0900

    placeholderの追加

--- next-app/src/components/layout/SearchBox.tsx
+++ next-app/src/components/layout/SearchBox.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
+
 export interface SearchBoxProps {
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     value: string;
+    placeholder: string;
 }
 
-export function SearchBox({ onChange, value }: SearchBoxProps) {
+export function SearchBox({ onChange, value, placeholder }: SearchBoxProps) {
     return (
         <div className="relative">
             <input 
@@ -12,7 +14,7 @@ export function SearchBox({ onChange, value }: SearchBoxProps) {
                 className="peer w-[500px] h-11 pl-8 pr-12 border border-textOpacity rounded-md text-text placeholder:text-base focus:outline-none focus:border-text"
                 onChange={onChange}
                 value={value}
-                placeholder="タイトルの検索が入ります"
+                placeholder={`${placeholder}の検索が入ります`}
             />
             <Image
                 className='absolute left-[460px] top-1/2 transform -translate-y-1/2 opacity-60 peer-focus:opacity-100'
