94620fed3ec11c78aaf6af2f0554df1daf8c6041
haruto8610 <haruto.fukushina0207@gmail.com>
2024-12-20 21:47:02 +0900

    削除モーダルの作成

--- next-app/src/app/top/page.tsx
+++ next-app/src/app/top/page.tsx
@@ -1,7 +1,11 @@
+import { DeleteModal } from "@/components/layout/DeleteModal";
+
 export default function Page() {
+    const list = ["fdlskjfslkjfslka", "dskfljdsfjadsklfjsadk", "dklfjasdklfjas"]
     return(
         <div>
             <h1>美味しい</h1>
+            
         </div>
     )
 }
