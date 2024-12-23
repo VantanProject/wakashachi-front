a16ddbc3fd1c8c9672e575edae1610752ad22a4f
haruto8610 <haruto.fukushina0207@gmail.com>
2024-12-23 13:16:23 +0900

    修正

--- next-app/src/components/layout/SearchBox.tsx
+++ next-app/src/components/layout/SearchBox.tsx
@@ -14,7 +14,7 @@ export function SearchBox({ onChange, value, placeholder }: SearchBoxProps) {
                 className="peer w-[500px] h-11 pl-8 pr-12 border border-textOpacity rounded-md text-text placeholder:text-base focus:outline-none focus:border-text"
                 onChange={onChange}
                 value={value}
-                placeholder={`${placeholder}の検索が入ります`}
+                placeholder={placeholder}
             />
             <Image
                 className='absolute left-[460px] top-1/2 transform -translate-y-1/2 opacity-60 peer-focus:opacity-100'
