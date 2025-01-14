83888130899183cf2b16761a55b7456d316131fe
Ryota-Okumuraa <okumurahaku3298@icloud.com>
2025-01-14 23:37:09 +0900

    gitの練習 (#13)
    * gitの練習
    
    * 英語に変更した

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -20,7 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>じゅんぺいのgit</li>
+          <li>jumpei's git</li>
+          <li>ryota's git</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
