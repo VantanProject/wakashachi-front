7bab6a6e490979a716da4cb24c91757002bf07d9
Kate0418 <nakao.keito0418@gmail.com>
2025-01-27 20:53:39 +0900

    リダイレクトの追加

--- next-app/src/app/merch/store/page.tsx
+++ next-app/src/app/merch/store/page.tsx
@@ -4,8 +4,10 @@ import { MerchStoreProps } from "@/api/MerchStore";
 import { MerchCustom } from "@/components/MerchCustom";
 import { useState } from "react";
 import { MerchStore } from "@/api/MerchStore";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+    const router = useRouter();
     const [merch, setMerch] = useState<MerchStoreProps["merch"]>({
         translations: Array.from({ length: 4 }, (_, i) => i).map((_, index) => {
             return {
@@ -22,6 +24,7 @@ export default function Page() {
         const response = await MerchStore({merch})
         if (response.success) {
             alert(response.message);
+            router.push("/merch");
         } else {
             alert(response.errors[0]);
         }
